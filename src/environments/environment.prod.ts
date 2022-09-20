462dbad043c230b77379783949fed57500437cc3
Giulia-M <giuliamaggio16@gmail.com>
2022-09-20 16:14:32 +0200

    dd

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyB7yqauVl8KAexl98vbINJBvcC6FPQWPFk',
+    authDomain: 'ecommerce-5bb3a.firebaseapp.com',
+    databaseURL: 'https://ecommerce-5bb3a-default-rtdb.firebaseio.com',
+    projectId: 'ecommerce-5bb3a',
+    storageBucket: 'ecommerce-5bb3a.appspot.com',
+    messagingSenderId: '300078053474',
+    appId: '1:300078053474:web:120506436184067b5fb93b',
+    measurementId: 'G-N3MX0SZPX7',
+  },
+};
